83e4cd3f58180a95283da9291042636a17e2e832
Xia junwe <jxia27@binghamton.edu>
2021-09-17 21:24:43 -0400

    First commit

---
+++ app/src/main/java/com/example/shoppinglist/CartAdapter.java
@@ -0,0 +1,117 @@
+package com.example.shoppinglist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class CartAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<HashMap<String,String>> dataList;
+    private ViewHolder vh;
+    private Map<String,Integer> pitchOnMap;
+    private RefreshPriceInterface refreshPriceInterface;
+
+    public CartAdapter(Context context, List<HashMap<String,String>> list){
+        this.context=context;
+        this.dataList=list;
+
+        pitchOnMap=new HashMap<>();
+        for(int i=0;i<dataList.size();i++){
+            pitchOnMap.put(dataList.get(i).get("id"),0);
+        }
+    }
+
+    @Override
+    public View getView(final int position, View view, ViewGroup viewGroup) {
+        vh=new ViewHolder();
+        if(view==null){
+            view= LayoutInflater.from(context).inflate(R.layout.item_shopcart_product,null);
+
+            vh.checkBox=(CheckBox)view.findViewById(R.id.check_box);
+            vh.icon=(ImageView)view.findViewById(R.id.iv_adapter_list_pic);
+            vh.name=(TextView)view.findViewById(R.id.tv_goods_name);
+            vh.price=(TextView)view.findViewById(R.id.tv_goods_price);
+            vh.type=(TextView)view.findViewById(R.id.tv_type_size);
+            vh.num=(TextView)view.findViewById(R.id.tv_num);
+            vh.reduce=(TextView)view.findViewById(R.id.tv_reduce);
+            vh.add=(TextView)view.findViewById(R.id.tv_add);
+
+            view.setTag(vh);
+        }else {
+            vh=(ViewHolder)view.getTag();
+        }
+
+        if(dataList.size()>0){
+
+            if(pitchOnMap.get(dataList.get(position).get("id"))==0)vh.checkBox.setChecked(false);
+            else vh.checkBox.setChecked(true);
+            HashMap<String,String> map=dataList.get(position);
+            vh.name.setText(map.get("name"));
+            vh.type.setText(map.get("type"));
+            vh.price.setText("$ "+(Double.valueOf(map.get("price"))));
+
+            vh.checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    final int index=position;
+                    if(((CheckBox)view).isChecked())pitchOnMap.put(dataList.get(index).get("id"),1);else pitchOnMap.put(dataList.get(index).get("id"),0);
+                    refreshPriceInterface.refreshPrice(pitchOnMap);
+                }
+            });
+        }
+
+        return view;
+    }
+
+    public Map<String,Integer> getPitchOnMap(){
+        return pitchOnMap;
+    }
+    public void setPitchOnMap(Map<String,Integer> pitchOnMap){
+        this.pitchOnMap=new HashMap<>();
+        this.pitchOnMap.putAll(pitchOnMap);
+    }
+
+    public interface RefreshPriceInterface{
+        void refreshPrice(Map<String,Integer> pitchOnMap);
+    }
+    public void setRefreshPriceInterface(RefreshPriceInterface refreshPriceInterface){
+        this.refreshPriceInterface=refreshPriceInterface;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public int getCount() {
+        if (dataList != null) {
+            return dataList.size();
+        } else {
+            return 0;
+        }
+    }
+
+    class ViewHolder{
+        CheckBox checkBox;
+        ImageView icon;
+        TextView name,price,num,type,reduce,add;
+    }
+
+}
---
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.shoppinglist;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements CartAdapter.RefreshPriceInterface ,View.OnClickListener{
+
+    private ListView listView;
+    private CheckBox cb_check_all;
+    private TextView tv_total_price;
+    private TextView tv_go_to_pay;
+
+    private CartAdapter adapter;
+
+    private double totalPrice = 0.00;
+    private int totalCount = 0;
+    private List<HashMap<String,String>> goodsList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two);
+
+        initDate();
+    }
+
+    private void priceControl(Map<String, Integer> pitchOnMap){
+        totalCount = 0;
+        totalPrice = 0.00;
+        for(int i=0;i<goodsList.size();i++){
+            if(pitchOnMap.get(goodsList.get(i).get("id"))==1){
+                totalCount=totalCount+Integer.valueOf(goodsList.get(i).get("count"));
+                double goodsPrice=Integer.valueOf(goodsList.get(i).get("count"))*Double.valueOf(goodsList.get(i).get("price"));
+                totalPrice=totalPrice+goodsPrice;
+            }
+        }
+        tv_total_price.setText("$ "+totalPrice);
+        tv_go_to_pay.setText("Pay：("+totalCount+")");
+    }
+
+    @Override
+    public void refreshPrice(Map<String, Integer> pitchOnMap) {
+        priceControl(pitchOnMap);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.all_chekbox:
+                AllTheSelected();
+                break;
+            case R.id.tv_go_to_pay:
+                if(totalCount<=0){
+                    Toast.makeText(this,"select product",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(this,"good",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    private void AllTheSelected(){
+        Map<String,Integer> map=adapter.getPitchOnMap();
+        boolean isCheck=false;
+        boolean isUnCheck=false;
+        Iterator iter = map.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            if(Integer.valueOf(entry.getValue().toString())==1)isCheck=true;
+            else isUnCheck=true;
+        }
+        if(isCheck==true&&isUnCheck==false){
+            for(int i=0;i<goodsList.size();i++){
+                map.put(goodsList.get(i).get("id"),0);
+            }
+            cb_check_all.setChecked(false);
+        }else if(isCheck==true && isUnCheck==true){
+            for(int i=0;i<goodsList.size();i++){
+                map.put(goodsList.get(i).get("id"),1);
+            }
+            cb_check_all.setChecked(true);
+        }else if(isCheck==false && isUnCheck==true){
+            for(int i=0;i<goodsList.size();i++){
+                map.put(goodsList.get(i).get("id"),1);
+            }
+            cb_check_all.setChecked(true);
+        }
+        priceControl(map);
+        adapter.setPitchOnMap(map);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void initView(){
+        listView = (ListView) findViewById(R.id.listview);
+        cb_check_all = (CheckBox) findViewById(R.id.all_chekbox);
+        tv_total_price = (TextView) findViewById(R.id.tv_total_price);
+        tv_go_to_pay = (TextView) findViewById(R.id.tv_go_to_pay);
+        tv_go_to_pay.setOnClickListener(this);
+        cb_check_all.setOnClickListener(this);
+
+        adapter=new CartAdapter(this,goodsList);
+        adapter.setRefreshPriceInterface(this);
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void initDate(){
+        goodsList=new ArrayList<>();
+        for(int i=0;i<5;i++){
+            HashMap<String,String> map=new HashMap<>();
+            map.put("id",(new Random().nextInt(10000)%(10000-2900+2900) + 2900)+"");
+            map.put("name","Cart "+(i+1)+" product");
+            map.put("price",(new Random().nextInt(100)%(100-29+29) + 29)+"");
+            map.put("count",("1"));
+            goodsList.add(map);
+        }
+
+        initView();
+    }
+
+}
